3abddc6ed9f725ab085a6208d2a3b39c17259797
Andjela Mircetic <andjelamircetic55@gmail.com>
2022-10-20 22:11:17 +0200

    Add administrator

---
+++ entities/administrator.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Administrator {
+    @PrimaryGeneratedColumn({
+name: 'administrator_id', type: 'int', unsigned: true
+    })
+    administratorId: number;
+
+    @Column({
+type: 'varchar', length: '32', unique:true
+    })
+    username: string;
+
+    @Column({
+        type: 'varchar', length: '128',  name:'password_hash'
+    })
+    passwordHash: string;
+}
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { Administrator } from 'entities/administrator.entity';
+
 import { AppService } from './app.service';
+import { AdministratorService } from './services/administrator/administrator.service';
 
 @Controller()
 export class AppController {
-  /*constructor(private readonly appService: AppService) {}*/
+  constructor(private administratorService: AdministratorService) {}
 
   /*@Get()
   getHello(): string {
@@ -13,6 +16,9 @@ export class AppController {
   getHello(): string {
     return  ' hey world!!!';
   }
- 
+  @Get('api/administrator')
+  getThem(): Promise<Administrator[]> {
+    return this.administratorService.getAll();
+  }
   
 }
